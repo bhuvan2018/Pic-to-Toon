ffe251351dc959c1c72e3abe9a675a06b7c6433b
Bhuvan_shetty <bhuvanshetty2018@gmail.com>
2025-03-13 17:51:57 +0530

    c

--- app.py
+++ app.py
@@ -165,7 +165,6 @@ def cartoonize():
                         cartoonized_video_url = f"/static/cartoonized/{video_filename}"
                     else:
                         video_url = upload_blob(video_path, "cartoonized_videos/" + video_filename)
-                        
                         response = api_request(video_url)
                         # Assume the API returns a URL to the cartoonized video
                         cartoonized_video_url = response.get("output_uri")
