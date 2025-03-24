ad1308dcf298a63ecb991035b1e9eaad433d51ac
Bhuvan_shetty <bhuvanshetty2018@gmail.com>
2025-03-24 22:40:55 +0530

    c

--- app.py
+++ app.py
@@ -34,7 +34,6 @@ app.config['OPTS'] = opts
 wb_cartoonizer = WB_Cartoonize(os.path.abspath("white_box_cartoonizer/saved_models/"), opts['gpu'])
 for folder in [app.config['UPLOAD_FOLDER_IMAGES'], app.config['UPLOAD_FOLDER_VIDEOS'], app.config['CARTOONIZED_FOLDER']]:
     os.makedirs(folder, exist_ok=True)
-
 def convert_bytes_to_image(img_bytes):
     """Convert image bytes to a NumPy array."""
     pil_image = Image.open(io.BytesIO(img_bytes))
